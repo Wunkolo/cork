d16bcd7429861be4e5bfc7374196e2de2bc8bcde
Wunkolo <Wunkolo@gmail.com>
2026-08-01 23:44:39 -0700

    Add x64 codeblock unit tests

--- tests/code_block.cpp
+++ tests/code_block.cpp
@@ -38,4 +38,37 @@ TEST_CASE("CodeBlock Execute a64", "[code_block]")
 
 	REQUIRE(Result == 0xDEADBEEF);
 }
+
+#elif defined(__x86_64__) || defined(_M_AMD64)
+// Testing codeblock execution on an x64 host.
+TEST_CASE("CodeBlock Execute x64", "[code_block]")
+{
+	// This is x64 assembly that is awkwardly aligned to 4-bytes
+	const std::array<std::uint32_t, 3> TestCode{{
+		0xadbeefb8,    // mov eax, 0xdeadbeef
+		0x90'90'c3'de, // ret, nop, nop
+	}};
+
+	std::optional<CodeBlock> CreateResult
+		= CodeBlock::Create(TestCode.size() * sizeof(std::uint32_t));
+	REQUIRE(CreateResult.has_value());
+
+	const CodeBlock& Code = CreateResult.value();
+	REQUIRE(Code.BlockMemory.data() != nullptr);
+
+	// Copy code over
+	std::copy(TestCode.cbegin(), TestCode.cend(), Code.BlockMemory.begin());
+
+	// Remove write access
+	Code.Protect();
+
+	// Invalidate i-cache
+	Code.Invalidate();
+
+	// Execute!
+	const std::size_t Result
+		= reinterpret_cast<std::size_t (*)()>(Code.BlockMemory.data())();
+
+	REQUIRE(Result == 0xDEADBEEF);
+}
 #endif
